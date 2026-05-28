521f51bf1c654cbbd7f043f1067bcda3f04c9e48
ManuelccFerreira <114316360+ManuelccFerreira@users.noreply.github.com>
2026-05-29 00:07:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {
   SMTP_SECURE = 'false',
   SMTP_USER = 'info@tg4travel.com',
   SMTP_PASS = 'rN(T;1~z2c&9',
-  SMTP_FROM, = 'TG4Travel <info@tg4travel.com>',
+  SMTP_FROM = 'TG4Travel <info@tg4travel.com>',
   BRIDGE_TOKEN = 'dad8d36b-543d-4b3b-93b0-a2faf03a8732dad8d36b-543d-4b3b-93b0-a2faf03a8732',
   PORT = '10000',
 } = process.env;
